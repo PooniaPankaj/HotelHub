b256e4c455af930797befd165a85b832aad9611c
Pankaj Poonia <pankajpoonia0703@gmail.com>
2023-06-05 19:18:30 +0530

    imported all useful libraries

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route } from "react-router-dom"
+import Home from './components/Home'
+import List from './components/list/List';
+import Hotel from './components/hotel/Hotel';
+function App() {
+  return (
+    <Routes>
+        <Route exact strict path="/" element={ <Home/> } />
+        <Route exact strict path="/hotels" element = {<List/>}></Route>
+        <Route exact strict path="/hotels/:id" element = {<Hotel/>}></Route>
+    </Routes>
+  );
+}
+
+export default App;
